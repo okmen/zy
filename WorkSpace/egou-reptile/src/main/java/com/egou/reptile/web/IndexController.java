af734d494108dc4c1d39736fbd977c4835021b3e
zy <okmen.zy@foxmail.com>
2016-11-23 17:56:54 +0800

    sss
    Signed-off-by: zy <okmen.zy@foxmail.com>

---
+++ WorkSpace/egou-reptile/src/main/java/com/egou/reptile/web/IndexController.java
@@ -0,0 +1,10 @@
+package com.egou.reptile.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/search")
+public class IndexController {
+
+}
